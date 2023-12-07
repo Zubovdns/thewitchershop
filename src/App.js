dd4d0f3a849be2ee5b8b2f9306a6642b78bf1f0a
ShellyKelly1 <issa_karim@mail.ru>
2023-12-07 16:15:44 +0300

    Added footer and repaired navgiation bar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import AboutUs from './components/about-us/AboutUs'
 import NavMenu from './components/nav_menu/NavMenu'
+import Footer from './components/footer/Footer'
 
 function App() {
 	return (
 		<div className='App'>
 			<NavMenu />
 			<AboutUs />
+			<Footer />
 		</div>
 	);
 }
---
+++ src/components/footer/Footer.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './css/Footer.css';
+
+function Footer() {
+	return (
+		<div className='Footer'>
+			<div className='Footer-content'>
+				<p className='Footer-on-top'>
+					<a className='Footer-on-top-href' href='#top'>
+						В начало
+					</a>
+				</p>
+			</div>
+		</div>
+	);
+}
+
+export default Footer;
