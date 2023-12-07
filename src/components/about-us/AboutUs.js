c4799fa85eb4fbce8f72ba41cead98c19fe45269
ShellyKelly1 <issa_karim@mail.ru>
2023-12-07 11:49:06 +0300

    Intermediate commit

--- src/components/about-us/AboutUs.js
+++ src/components/about-us/AboutUs.js
@@ -1,15 +1,15 @@
 import React, { useEffect, useState } from "react";
 import "./css/AboutUs.css";
-import ImageCall from './img/ImageCall.png';
-import ImageTG from './img/ImageTG.png';
-import ImageVK from './img/ImageVK.png';
+import ImageCall from "./img/ImageCall.png";
+import ImageTG from "./img/ImageTG.png";
+import ImageVK from "./img/ImageVK.png";
 
 function AboutUs() {
   return (
     <div className="AboutUs">
       <div className="AboutUsHeader">О нас</div>
       <div className="AboutUsInfo">
-        <p>Добро пожаловать на страницу нашего веб-сайта!</p>{" "}
+        <p className='InfoHeader'>Добро пожаловать на страницу нашего веб-сайта!</p>{" "}
         <p>
           Мы - студенческий коллектив, объединенный общим интересом к учебному
           процессу и стремлением достичь лучших результатов.
@@ -25,16 +25,33 @@ function AboutUs() {
         </p>
         <div className="AboutUsInfoHeader">Связаться с нами</div>
         <div className="FirstContact">
-			<div className='FirstImageCall'>
-				<img className='FirstImageCallPng' src={ImageCall}/>
-				<p>+375 (29) 572-80-44</p>
-			</div>
-			<div className='FirstImageVK'>
-				<img className='FirstImageCallPng' src={ImageCall}/>
-				<p>+375 (29) 572-80-44</p>
-			</div>
-		</div>
-        <div className="SecondContact">example</div>
+          <div className="ImageCall">
+            <img className="ImageCallPng" src={ImageCall} />
+            <p>+375 (29) 572-80-44</p>
+          </div>
+          <div className="ImageVK">
+            <img className="ImageCallPng" src={ImageVK} />
+            <p>galaxyeater</p>
+          </div>
+          <div className="ImageTG">
+            <img className="ImageCallPng" src={ImageTG} />
+            <p>savagemercury</p>
+          </div>
+        </div>
+        <div className="SecondContact">
+          <div className="ImageCall">
+            <img className="ImageCallPng" src={ImageCall} />
+            <p>+375 (44) 582-53-21</p>
+          </div>
+          <div className="ImageVK2">
+            <img className="ImageCallPng" src={ImageVK} />
+            <p>zubovdns</p>
+          </div>
+          <div className="ImageTG">
+            <img className="ImageCallPng" src={ImageTG} />
+            <p>zubovdns</p>
+          </div>
+        </div>
       </div>
     </div>
   );
