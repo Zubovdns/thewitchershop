351a47fbb2415b2fa243d4707ffe5aaec9d56594
Zubovdns <zubovdns@gmail.com>
2023-12-08 08:11:06 +0300

    Intermediate commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import './App.css';
 import Footer from './components/footer/Footer';
-import MainScreen from './components/main_screen/MainScreen';
 import NavMenu from './components/nav_menu/NavMenu';
+import ProductPage from './components/product_page/ProductPage';
 
 function App() {
 	return (
 		<div className='App'>
 			<NavMenu />
-			<MainScreen />
+			<ProductPage />
 			<Footer />
 		</div>
 	);
---
+++ src/components/product_page/ProductPage.js
@@ -0,0 +1,86 @@
+import React from 'react';
+import './css/ProductPage.css';
+import placeholder from './img/Placeholder_img.png';
+import instagramIcon from './img/instagram_img.png';
+import telegramIcon from './img/telegram_img.png';
+import vkIcon from './img/vk_img.png';
+
+function ProductPage() {
+	return (
+		<div className='ProductPage'>
+			<div className='ProductPage-body'>
+				<div className='ProductPage-label'>
+					<a className='ProductPage-label-text'>Кружка “Белый Волк”</a>
+				</div>
+				<div className='ProductPage-path'>
+					<a className='ProductPage-path-text'>
+						Главная {'>'} Категории {'>'} Кружки
+					</a>
+				</div>
+				<div className='ProductPage-product-card'>
+					<div className='ProductPage-product-body'>
+						<div className='ProductPage-product-div-image'>
+							<img className='ProductPage-product-image' src={placeholder} />
+						</div>
+						<div className='ProductPage-product-description'>
+							<a className='ProductPage-product-description-label'>Описание</a>
+							<a className='ProductPage-product-description-text'>
+								Few things are more dangerous than a hungry cyberpunk — perhaps
+								only a cyberpsycho close to the edge! So, if you’ve been
+								searching Night City for a byte-sized, high-tech snack, then
+								this 1.5” zinc alloy wonder has you covered.
+							</a>
+							<a className='ProductPage-product-description-text'>
+								This mouth-watering pin resembles a little-known delicacy of the
+								dark future — one that would even tempt sleeper agent Solomon
+								Reed out of the shadows.
+							</a>
+							<a className='ProductPage-product-description-text'>
+								Whether you're a data-hungry netrunner or a flavor-loving street
+								samurai, the Cyberpunk 2077: Cyber Quesadilla Pin is a must-have
+								addition to your collection. Get ready to embrace your inner
+								cyber-foodie!
+							</a>
+						</div>
+					</div>
+					<div className='ProductPage-product-block'>
+						<div className='ProductPage-product-buy'>
+							<div className='ProductPage-product-buy-content'>
+								<a className='ProductPage-product-buy-cost'>$14.99</a>
+								<a className='ProductPage-product-buy-width'>Ширина: 30см</a>
+								<a className='ProductPage-product-buy-height'>Высота: 25см</a>
+								<a className='ProductPage-product-buy-availability'>
+									Наличие: отсутствует
+								</a>
+							</div>
+
+							<input className='ProductPage-product-buy-amount'></input>
+							<button className='ProductPage-product-buy-button'>
+								Добавить в корзину
+							</button>
+						</div>
+						<div className='ProductPage-product-share'>
+							<a className='ProductPage-product-share-label'>Поделиться</a>
+							<div className='ProductPage-product-share-links'>
+								<img className='ProductPage-product-share-icon' src={vkIcon} />
+								<img
+									className='ProductPage-product-share-icon'
+									src={telegramIcon}
+								/>
+								<img
+									className='ProductPage-product-share-icon'
+									src={instagramIcon}
+								/>
+							</div>
+							<a className='ProductPage-product-share-shared-amount'>
+								Уже поделилось: 1513
+							</a>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+}
+
+export default ProductPage;
