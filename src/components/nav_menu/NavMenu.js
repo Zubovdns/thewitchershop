3493b03f8a07ccfd0582bbc6e477e84cb1db75e1
Zubovdns <zubovdns@gmail.com>
2023-12-08 08:23:53 +0300

    hotfix

--- src/components/nav_menu/NavMenu.js
+++ src/components/nav_menu/NavMenu.js
@@ -1,14 +1,7 @@
-<<<<<<< HEAD
 import React, { useEffect, useState } from 'react';
 import './css/NavMenu.css';
 import basketImg from './img/basket_img.png';
 import witcherEmblemImg from './img/witcher_emblem_img.png';
-=======
-import React, { useEffect, useState } from "react";
-import "./css/NavMenu.css";
-import basketImg from "./img/basket_img.png";
-import witcherEmblemImg from "./img/witcher_emblem_img.png";
->>>>>>> about-us
 
 function NavMenu() {
 	const [isSticky, setIsSticky] = useState(false);
@@ -32,7 +25,6 @@ function NavMenu() {
 		};
 	}, []);
 
-<<<<<<< HEAD
 	return (
 		<div className={`NavMenu ${isSticky ? 'sticky' : ''}`}>
 			{imageUrl && <img className='NavMenu-image' src={imageUrl} />}
@@ -51,26 +43,6 @@ function NavMenu() {
 			</a>
 		</div>
 	);
-=======
-  return (
-    <div className={`NavMenu ${isSticky ? "sticky" : ""}`}>
-      {imageUrl && <img className="NavMenu-image" src={imageUrl} />}
-      <a className="NavMenu-text" href="">
-        Главная
-      </a>
-      <a className="NavMenu-text" href="">
-        Категории
-      </a>
-      <a className="NavMenu-text" href="">
-        О нас
-      </a>
-      <input type="text" className="NavMenu-input" />
-      <a href="">
-        <img className="NavMenu-icon" src={basketImg} />
-      </a>
-    </div>
-  );
->>>>>>> about-us
 }
 
 export default NavMenu;
