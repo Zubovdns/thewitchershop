9e0a9c00b2516ee644a36a53914f91064f22df88
ShellyKelly1 <issa_karim@mail.ru>
2023-12-14 10:32:39 +0300

    Modified footer

--- src/pages/footer/Footer.js
+++ src/pages/footer/Footer.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import './css/Footer.css';
+import witcherEmblemImg from '../main_screen/img/witcher_emblem_img.png'
 
 function Footer() {
 	return (
 		<div className='Footer'>
 			<div className='Footer-content'>
-				<p className='Footer-on-top'>
-					<a className='Footer-on-top-href' href='#top'>
-						В начало
-					</a>
-				</p>
+				<div className='Footer-content-imageAndName'>
+					<img className='witcherEmblem' src={witcherEmblemImg}></img>
+					<p className='textStyle'>The Witcher Shop</p>
+				</div>
+				<div className=''></div>
 			</div>
 		</div>
 	);
