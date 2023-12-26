989da1b4ede02aa1af035fbf76ac5ce2dc53d673
ShellyKelly1 <issa_karim@mail.ru>
2023-12-26 12:10:18 +0300

    Intermediate commit

--- src/pages/footer/Footer.js
+++ src/pages/footer/Footer.js
@@ -1,19 +1,25 @@
-import React from 'react';
-import './css/Footer.css';
-import witcherEmblemImg from '../main_screen/img/witcher_emblem_img.png'
+import React from "react";
+import "./css/Footer.css";
+import witcherEmblemImg from "../main_screen/img/witcher_emblem_img.png";
 
 function Footer() {
-	return (
-		<div className='Footer'>
-			<div className='Footer-content'>
-				<div className='Footer-content-imageAndName'>
-					<img className='witcherEmblem' src={witcherEmblemImg}></img>
-					<p className='textStyle'>The Witcher Shop</p>
-				</div>
-				<div className=''></div>
-			</div>
-		</div>
-	);
+  return (
+    <div className="Footer">
+      <div className="Footer-content">
+        <div className="Footer-content-imageAndName">
+          <img className="witcherEmblem" src={witcherEmblemImg}></img>
+          <p className="textStyle">The Witcher Shop</p>
+        </div>
+        <div className="Footer-content-right">
+          <p className="Footer-content-right-topText">
+            Мост между вашим желанием и множеством возможностей, где качество
+            встречает удобство, а выбор становится персональным приключением
+          </p>
+		  <hr className="Footer-content-right-line"></hr>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Footer;
