1acae8304c31e4be3312a06896312683d84c9b58
ShellyKelly1 <issa_karim@mail.ru>
2023-12-27 09:29:01 +0300

    Intermediate commit

--- src/pages/footer/Footer.js
+++ src/pages/footer/Footer.js
@@ -1,6 +1,9 @@
 import React from "react";
 import "./css/Footer.css";
 import witcherEmblemImg from "../main_screen/img/witcher_emblem_img.png";
+import ImageCall from "./img/ImageCall.png";
+import ImageVK from "./img/ImageVK.png";
+import ImageTG from "./img/ImageTG.png";
 
 function Footer() {
   return (
@@ -15,7 +18,21 @@ function Footer() {
             Мост между вашим желанием и множеством возможностей, где качество
             встречает удобство, а выбор становится персональным приключением
           </p>
-		  <hr className="Footer-content-right-line"></hr>
+          <hr className="Footer-content-right-line"></hr>
+          <div className="Footer-content-right-bottom">
+            <div className="ImageCall">
+              <img className="ImageCallPng" src={ImageCall} />
+              <p>The Witcher Shop</p>
+            </div>
+            <div className="ImageVK">
+              <img className="ImageCallPng" src={ImageVK} />
+              <p>The Witcher Shop</p>
+            </div>
+            <div className="ImageTG">
+              <img className="ImageCallPng" src={ImageTG} />
+              <p>The Witcher Shop</p>
+            </div>
+          </div>
         </div>
       </div>
     </div>
